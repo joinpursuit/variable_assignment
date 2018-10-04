92bac6821f119fd64200b3431a54c395e43e07db
godsby <gg4biz@gmail.com>
2018-10-04 19:25:59 -0400

    Well done variable_assignment

---
+++ variable_solution.js
@@ -0,0 +1,43 @@
+//Question 1
+/*
+let birthYear = 1981,
+    futureYear = 2028,
+    ageMin = futureYear - birthYear - 1,
+    ageMax = futureYear - birthYear;
+console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear);
+
+//Question 2
+
+let currentAge = 37;
+    maxAge = 80;
+    snackAmount = 2;
+    totalSnack = (maxAge - currentAge) * 2;
+console.log("You will need " + totalSnack + " to last you until the age of " + maxAge);
+
+//Question 3
+
+let radius = 3;
+    circumferenceResult = Math.PI * radius * 2;
+    areaOfCircle = Math.PI * radius * radius;
+console.log("The circumference is " + circumferenceResult.toFixed(2));
+console.log("The area is " + areaOfCircle.toFixed(2));
+
+//Question 4
+
+let tempInCelcius = 40;
+    tempInFahrenheit = tempInCelcius * 9 / 5 + 32;
+console.log(tempInFahrenheit + "°F" + " is " + tempInCelcius + "°C.");
+
+//Question 5
+
+10
+
+//Question 6
+
+jello
+
+//Question 7
+
+string
+
+*/
