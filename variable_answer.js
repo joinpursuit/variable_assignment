afde6a1f499495b80da0f02f5fa43a1ab413858e
Morteza Khaki <mortezakhaki@Mortezas-MacBook-Air.local>
2018-10-04 19:10:22 -0400

    variable

---
+++ variable_answer.js
@@ -0,0 +1,36 @@
+//1.
+// let myBirthYear = 1979
+// let futureYear = 2050
+// let ageMax = futureYear - myBirthYear
+// let ageMin = ageMax - 1
+// console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear)
+
+//2.
+// let myAge = 39
+// let maxAge = 100
+// let myTime = maxAge - myAge
+// let snackPerDay = 3
+// let everyYear = 365
+// let snacksPerYear = snackPerDay * everyYear
+// let snackNumber = snacksPerYear * myTime
+// console.log("You will need " + snackNumber + " to last you until the age of " +maxAge)
+
+//3.
+// let myRadius = 3
+// let circumferenceResult = 2 * Math.PI * myRadius
+// console.log("The circumference is " + circumferenceResult)
+// let areaOfCircle = 2 * Math.PI * Math.pow(myRadius, 2)
+// console.log("The circumference is " + areaOfCircle)
+
+//4
+// let tempInCelcius = 20
+// let tempInFahrenheit = (tempInCelcius * 9/5 ) + 32
+// console.log(tempInCelcius + " °C is " + tempInFahrenheit + " °F " );
+
+// let tempInFahrenheit = 69
+// let tempInCelcius = (tempInFahrenheit * 5/9 ) - 32
+// console.log(tempInFahrenheit + " °F is " + tempInCelcius + " °C " )
+
+//5. 10
+//6. jello
+//7. string
