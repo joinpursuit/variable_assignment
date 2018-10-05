56c8d70527c6750ef3e82ec4fecc1fc1b8ca441c
Nicolle Loayza <nicolleloayza@Nicolles-MacBook-Pro.local>
2018-10-04 21:46:06 -0400

    Variable Assigment

---
+++ variable_assignment.js
@@ -0,0 +1,38 @@
+/*1.
+var birthAge = 1990
+var futureYear = 2020
+
+let futureAge = futureYear - birthAge
+console.log(futureAge)
+*/
+
+/*2.
+var currentAge = 28
+var maxAge = 100
+var snackNumber = 3
+
+let numberOfSnacks = (maxAge - currentAge) * snackNumber
+console.log(numberOfSnacks)
+*/
+
+/*3.
+var pI = 3.141592;
+var radius = 2;
+var diameter = 2 * radius;
+var circumferenceResult = pI * diameter;
+var areaOfCircle = pI * radius * radius;
+
+console.log(areaOfCircle)
+*/
+
+/*4.
+var tempInCelcius = 20;
+var tempInFahrenheit = tempInCelcius * 9/5 + 32;
+console.log(tempInCelcius + '°C is ' + tempInFahrenheit + '°F ')
+*/
+
+//5. 10
+
+//6. jello
+
+//7. string
