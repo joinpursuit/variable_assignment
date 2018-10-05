f6809317ff1189518a5cf39817dea881a70111a2
Andres.C <andrescabrera@pursuit.org>
2018-10-04 23:30:52 -0400

    variables hw

---
+++ variableHW.js
@@ -0,0 +1,36 @@
+// 1. //age Calculator
+// const myBirthYear = 1990
+// let futureYear = 2018
+// let ageMax= futureYear - myBirthYear
+// let ageMin= ageMax - 1
+// console.log ( ' I will be either ' + ageMin + ' or ' + ageMax + 'in' + futureYear)
+
+
+// const myAge= 28
+// let maxAge = 31
+// let snckAmnt = 99
+// let ageDiff = maxAge - myAge
+// let snckEats = snckAmnt / ageDiff
+// console.log ( "You will need " + snckEats +  " snacks to last you until the age of " + maxAge + ".")
+
+
+//3. Cirle Stuffs
+// let radius = 4
+// let circumferenceResult = 2 * Math.PI * radius
+// console.log ( "The circumference result is " + circumferenceResult)
+// let areaOfCircle = Math.PI * Math.pow (radius,2);
+// console.log ( "The area of a cirle is" + areaOfCircle)
+
+// 4. temperature
+// let tempInCelcius = 5
+// let tempInFahrenheit = (tempInCelcius * 9/5) + 32
+// console.log (tempInCelcius + ' °C ' + 'is output to ' + tempInFahrenheit + '°F')
+
+// let tempInFahrenheit = 5
+// let tempInCelcius = (tempInFahrenheit-32) * 5/9
+// console.log (tempInFahrenheit + ' °F ' + 'is output to ' + tempInCelcius + '°C')
+
+//5 Questions 
+// 10
+// jello
+// My favorite number is
