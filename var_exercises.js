07b70ddd088ddd2081a6143bd294ff29e0d1bb30
Pixelynx <tjstevens1992@gmail.com>
2018-10-05 00:20:02 -0400

    TKeya Stevens' var exercises

---
+++ var_exercises.js
@@ -0,0 +1,48 @@
+// Question 1:
+
+const birthYear = 1992;
+let futYear = 2037;
+let ageMax = 45;
+let ageMin = 44;
+
+console.log('I will be either ' + ageMin + ' or ' + ageMax + ' in ' + futYear + '.');
+
+// Question 2:
+
+let curAge = 25;
+let maxAge = 60;
+let snackAmm = 3;
+let ageDiff = maxAge - curAge;
+let snkNeed = ageDiff * snackAmm;
+
+console.log('I will need ' + snkNeed + ' snacks to last me until the age of ' + maxAge + '!');
+
+// Question 3:
+
+let radius = 12;
+const cir = Math.floor(2 * Math.PI * radius);
+const area = Math.floor(radius * radius * Math.PI);
+
+console.log('The circumference is ' + cir + '.');
+console.log('The area is ' + area + '.');
+
+// Question 4:
+
+let c = 24;
+let f = 75;
+const toCel = Math.floor((f - 32) * 5/9);
+const toFahr = Math.floor(9/5 * c + 32);
+
+console.log(f + '°F' + ' is ' + toCel + '°C.');
+console.log(c + '°C' + ' is' + toFahr + '°F.');
+
+
+
+// Question 5:
+  // 10
+
+// Question 6:
+  // jello
+
+// Question 7:
+  // string
