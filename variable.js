d1c692b9a461822435b2688c1a7f15957a0feab2
Pearl <pearljeanpierre@pursuit.org>
2019-06-29 18:50:42 -0400

    variable_lab

---
+++ variable.js
@@ -0,0 +1,58 @@
+const birthYear=1950;
+
+let futureYearplusOne=2001;
+let futureYear=2000;
+
+
+console.log(futureYearplusOne-birthYear);
+console.log(futureYear-birthYear);
+
+let answer = "I will be " + (futureYearplusOne-birthYear) + " or " + (futureYear-birthYear) + " in " + futureYearplusOne + " or " + futureYear
+console.log(answer);
+
+const currentAge = 50;
+let maxAge = 80;
+let snackPerday = 3;
+console.log((maxAge-currentAge)*3*365)
+
+let answer = " You will need " + ((maxAge - currentAge)*3*365) + " to last you until the age of" + " " + (maxAge)
+console.log(answer);
+
+
+let radius =3;
+
+let circumferenceResult = (2* radius) *Math.PI;
+console.log(Math.PI)
+console.log(circumferenceResult);
+
+
+let radius =3;
+
+let areaOfcircle = Math.PI* radius *radius
+console.log(areaOfcircle)
+
+let tempIncelsius=0;
+let tempInfahrenheit=tempIncelsius *9/5 +32
+console.log(tempInfahrenheit)
+
+let tempInfahrenheit=33;
+let tempIncelsius=tempInfahrenheit-32 *5/9
+console.log(tempIncelsius)
+
+
+let num1 = 2;
+let num2 = 5;
+let num3 = num1 * num2;
+console.log(num3); //10-My answer was correct 2*5 =10
+
+
+let str = 'jel'
+str += 'lo'
+console.log(str)//jello-My answer was correct, it is a String
+
+
+let string =  'My favorite number is ';
+let number = 42
+let sentence = string + number
+console.log(typeof(sentence))// My favorite number is 42, it is a string, so the comment is combined. My answer was correct
+//Variable lap assignment done by Pearl Jean-Pierre
