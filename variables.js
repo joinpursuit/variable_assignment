b811127d925520f137fa42a8b9d66922fa21a185
DejaLFlynn <dejaflynn@pursuit.org>
2019-06-29 17:54:36 -0400

    answers to variables

---
+++ variables.js
@@ -0,0 +1,74 @@
+/* 1. Age Calculator:
+    * Store your birth year in a variable.
+    * Store a future year in a variable.*/
+    /* Calculate your 2 possible ages for that year based on the stored values. */
+    /* For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.*/
+    //* Log them to the screen like so: "I will be either `ageMin` or `ageMax` in `futureYear`", substituting the values. */
+
+    const dob = 1981;
+    let fy = 2050;
+    myAge = Date ()-dob
+    ageMax = fy - dob;
+    ageMin = fy - dob - 1;
+    console.log('I will be either '+ ageMin + ' for ' + ageMax +' in futureYear');
+
+/* . Snack Supply Calculator: */
+    //* Store your current age in a variable.
+    //* Store a maximum age in a variable.
+    //* Store an estimated snack amount per day (as a number).
+     //* Calculate how many snacks you would eat total, from your current age until the maximum age.
+    //* Log the result to the screen like this: "You will need `snackNumber` to last you until the age of `maxAge`".
+
+    const nowAge = 38;
+    let maxAge = 55;
+    snackNumber = 2;
+    totalSnacks = (maxAge - nowAge) * snackNumber;
+    console.log('Need ' +  totalSnacks + " which is MAAADDD SNACKS TO LAST ME");
+
+//3. Calculate properties of a circle, using the definitions [here](http://math2.org/math/geometry/circles.htm).
+    //* Use google: find how to get the number `PI` in JavaScript.
+    //* Store a radius into a variable.
+    //* Calculate the circumference based on the radius, and log "The circumference is `circumferenceResult`".
+    //* Calculate the area based on the radius, and log "The area is `areaOfCircle`".
+
+    let radius = 5;
+    let circumferenceResult = Math.PI * radius * 2
+    console.log("circumferenceResult" + circumferenceResult);
+  let areaOfCircle = Math.PI * Math.pow(radius,2);
+  console.log('the area is ' + areaOfCircle);
+  //4. Temperature Converter:
+      //* Store a celsius temperature into a variable.
+      //* Convert it to fahrenheit and output "`tempInCelcius`°C is `tempInFahrenheit`°F".
+      //* Now store a fahrenheit temperature into a variable.
+      //* Convert it to celsius and output "`tempInFahrenheit`°F is `tempInCelcius`°C."
+      let tempInCelcius = 0;
+      let tempInFahrenheit = 32;
+      console.log(tempInCelcius + " °C is" + tempInFahrenheit + " °F");
+      let celsiusResult = 1
+      let fahrenheitResult = 2
+      celsiusResult =  fahrenheitResult * (9 / 5 ) + 32;
+      console.log(celsiusResult + "°F " + fahrenheitResult + "°C")
+
+      5.
+
+          let num1 = 2
+          let num2 = 5
+          let num3 = num1 * num2
+          console.log(num3)//  10
+          //my prediction was correct
+
+      6.
+
+          let str = 'jel'
+          str += 'lo'
+          console.log(str)//jello
+          //my prediction is correct
+
+
+      7.
+
+          let string =  'My favorite number is ';
+          let number = 42
+          let sentence = string + number
+          console.log(typeof(sentence))
+          // my prediction is favorite number is 42");
