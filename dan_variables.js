455b3bb7c71175c59d2e9bc65705fb773617c4df
RougeLight <dantaeflowers@pursuit.org>
2019-06-30 16:57:47 -0400

    assignment

---
+++ dan_variables.js
@@ -0,0 +1,30 @@
+//const birthyear = 1986;
+//let futureyear  = 2055;
+//let ageMin    = futureyear - birthyear;
+//const ageMax    = futureyear - birthyear + 1;
+//console.log('age is' + ' ' + ageMin + ' ' + 'or' + ' ' + ageMax);
+
+//const age = 22;
+//let ageMax = 40;
+//let snack = (ageMax - age);
+//snack = snack * 365;
+//console.log('Total snack until age' + ' ' + ageMax + ' ' + 'is' + ' ' + snack);
+
+//let radius = 4;
+//let circ = 2 * Math.PI * radius;
+//console.log('Circumference is' + ' ' + circ);
+
+//let tempInCelcius = 50;
+//let tempInFahrenheit = tempInCelcius * 1.8 + 32;
+
+//console.log(tempInCelcius + 'celcius' + ' ' + 'is' + ' ' + tempInFahrenheit + ' Fahrenheit');
+
+//tempInFahrenheit = 500;
+//tempInCelcius = tempInFahrenheit / 1.8 - 32;
+
+//console.log(tempInFahrenheit + ' Fahrenheit' + ' ' + 'is' + ' ' + tempInCelcius + ' celcius');
+
+let string = 'My favorite number is ';
+let number = 42;
+let sentence = string + number;
+console.log(typeof (sentence);
