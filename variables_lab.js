856edb55597f62996fe16bc0b07066b612fd321b
Dugmar <dugmar.morocho@gmail.com>
2019-09-28 16:38:13 -0400

    Problem set 1 and 2

--- variables_lab.js
+++ variables_lab.js
@@ -1,29 +1,24 @@
-let birthYear = 1995;
-let futureYear = 2050;
-let ageMin =  futureYear - birthYear;
-let ageMax =  ageMin - 1;
-console.log('I will either be ' + ageMin + ' or ' + ageMax + ' in ' + futureYear);
 
-// Snack supply calculator
-let crrAge = 23;
-let maxAge = 50;
-let snackDay = 1;
-let favSanck = 'Peanuts';
-let snackYear = snackDay * 365;
-let totalSnack = snackYear * (maxAge - crrAge);
-console.log('You will need ' + totalSnack + ' of ' + favSanck + ' to last you until the age of ' + maxAge); 
+// Problem Set one
 
-//Properties of a circle
-let radius = 7
-let diameter = radius * 2
-let circumference = Math.PI * diameter 
-let area = Math.PI * radius * radius
-console.log('The circumference is: ' + circumference);
-console.log('The area is: ' + area);
+let birthYear = 1994;
+let futureYear = 2020;
+let ageMax =  futureYear - birthYear  ;
+let ageMin = ageMax -1;
 
-// Temperature converter
-let c = 0;
-let cInF = (c * 1.8) + 32;
-let f = 32;
-let fInC = (f - 32) / 1.8;
-console.log(`C = ${c} into F is: ${cInF} ---- F = ${f} into C is: ${fInC}`)
+console.log ("I will be either "+ ageMax + "or "+ ageMin + "in"+ futureYear);
+
+// Problem Set two
+
+let currentAge = 25;
+let maxAge = 55;
+let estimatedSnack = 5;
+let daysYear = 365;
+let snackNumber = (maxAge - currentAge) * daysYear;
+
+console.log ("You will need "+ snackNumber+" to last you until the age of "+maxAge);
+
+// Problem Set three
+
+
+
