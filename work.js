4e2f6ff9606a1fb87c117438b92ae05a262d2370
Oblivion <kiaraedwards@pursuit.org>
2019-09-28 17:41:51 -0400

    finished

---
+++ work.js
@@ -0,0 +1,61 @@
+// let birthYear = 1999;
+// let  futureYear = 2026;
+// let ageMin = futureYear - birthYear;
+// let ageMax = ageMin - 1;
+// console.log("i will be" + " " + ageMin + " "+ "or" + " " + ageMax + " "  + "in" + " " + futureYear);
+
+
+// const crrAge = 20;
+// const maxAge = 30;
+// const favSnack = "oreos"
+// let snackDay = 2;
+// let year = 365;
+// console.log("i will need" + " " + snackDay * year + " " + favSnack + " " + "to last me till" + " " + maxAge);
+
+
+
+// const radius = 10;
+// let diameter = radius * 2;
+// let circumference = Math.PI * diameter;
+// let area = Math.PI * radius * radius;
+// console.log('The circumference is: ' + circumference);
+// console.log('The area is: ' + area);
+
+// let c = 2;
+// let cInF = (c * 3) + 4;
+// let f = 4;
+// let fInC = (f - 4) / 3;
+// console.log(`C = ${c} into F is: ${cInF} ---- F = ${f} into C is: ${fInC}`)
+
+
+
+
+// let alice = 50;
+// let bob = 90;
+// let cam = 70;
+// let avg = 50 + 90 + 70 / 3;
+// console.log(avg)
+// let dee = 65;
+// let avg2 = 50 + 90 + 70 + 65 / 4;
+// console.log(avg2);
+
+// console.log("cat")
+
+let num1 = 2 
+let num2 = 5 
+let num3 = num1 * num2 
+// 10
+console.log(num3)
+
+
+let str = 'jel' 
+str += 'lo' 
+// jello
+console.log(str)
+
+let string = 'My favorite number is '; 
+let number = 42 
+let sentence = string + number 
+// string
+console.log(typeof(sentence))
+
