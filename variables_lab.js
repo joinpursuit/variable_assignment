3ccab3a7d296806fa8c705c082a9e8b6e3751b0a
Dugmar <dugmar.morocho@gmail.com>
2019-09-28 17:39:14 -0400

    Problem Solutions

--- variables_lab.js
+++ variables_lab.js
@@ -20,5 +20,28 @@ console.log ("You will need "+ snackNumber+" to last you until the age of "+maxA
 
 // Problem Set three
 
+let radius = 5
+let diameter = radius * 2
+let circumferenceResult = Math.PI * diameter
+let areaOfCircle = Math.PI* Math.pow(radius,2)
 
+console.log ("The circumference is " + circumferenceResult)
+console.log ("The area is " + areaOfCircle)
+
+// Problem Set Four
+let tempInCelcius = 25;
+let tempInFahrenheit = tempInCelcius * (9/5) + 32;
+
+console.log (tempInCelcius+ "°C is "+tempInFahrenheit+"°F");
+
+let tempInFahrenheitTwo = 25;
+let tempInCelciusTwo = (tempInFahrenheitTwo - 32) * 5/9 ;
+
+console.log (tempInFahrenheitTwo + "°F is "+ tempInCelciusTwo + "°C." );
+
+// Problem set Six
+
+// a. 10
+// b. jello
+// c. string 
 
