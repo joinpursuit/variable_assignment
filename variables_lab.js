8206b1009635b5cada0769762b0432877a9207e8
Danielle R Cherry <daniellecherry@pursuit.org>
2019-09-28 19:43:52 -0400

    complete

--- variables_lab.js
+++ variables_lab.js
@@ -1,29 +1,77 @@
-let birthYear = 1995;
-let futureYear = 2050;
-let ageMin =  futureYear - birthYear;
-let ageMax =  ageMin - 1;
-console.log('I will either be ' + ageMin + ' or ' + ageMax + ' in ' + futureYear);
+// ## Problem One
+var birthYear = 1991;
+var futureYear = 2056;
+console.log(birthYear - futureYear)
 
-// Snack supply calculator
-let crrAge = 23;
-let maxAge = 50;
-let snackDay = 1;
-let favSanck = 'Peanuts';
-let snackYear = snackDay * 365;
-let totalSnack = snackYear * (maxAge - crrAge);
-console.log('You will need ' + totalSnack + ' of ' + favSanck + ' to last you until the age of ' + maxAge); 
+ageMax = futureYear - birthYear;
+ageMin = futureYear - birthYear - 1;
 
-//Properties of a circle
-let radius = 7
-let diameter = radius * 2
-let circumference = Math.PI * diameter 
-let area = Math.PI * radius * radius
-console.log('The circumference is: ' + circumference);
-console.log('The area is: ' + area);
+console.log('I will be either ' + ageMin + ' or ' + ageMax);
 
-// Temperature converter
-let c = 0;
-let cInF = (c * 1.8) + 32;
-let f = 32;
-let fInC = (f - 32) / 1.8;
-console.log(`C = ${c} into F is: ${cInF} ---- F = ${f} into C is: ${fInC}`)
+
+// ## Problem Two
+let currentAge = 28;
+let maxAge = 36;
+let appAmountSnacks = 2;
+let snackS = appAmountSnacks * 7; 
+let snackS2 = snackS * 4;
+// console.log(snackS2)
+let snackSS = snackS2 * 12;
+// console.log(snackSS)
+let snackYear = snackSS;
+let snackNumber = snackYear * currentAge + 36;
+// console.log(snackNumber)
+
+console.log('You will need ' + snackNumber + ' to last you until the age of ' + maxAge);
+
+//  Problem Three
+let radius = 2 * 3.14159;
+let circumferenceResult = radius * 14; 
+
+console.log('The area is ' + circumferenceResult);
+
+let circumferenceResult2 = Math.pow(circumferenceResult, 2)
+let areaOfCircle = circumferenceResult2 / 4 * 3.14159
+console.log("The area is " + areaOfCircle)
+
+//Problem Four
+let celciusTemp = 45;
+let tempInFahrenheit = (celciusTemp * 9/5) + 32;
+
+console.log(celciusTemp + "°C is " + tempInFahrenheit + " °F");
+
+ let fahrenheitTemp = 200;
+ let tempInCelcius = (fahrenheitTemp - 32) * 5/9;
+ console.log(fahrenheitTemp + "°F is " + tempInCelcius + "°C.");
+
+ //Problem 5
+let aliceGrade = 89;
+let bobGrade = 75;
+let camGrade = 85;
+let gradeAverage = (aliceGrade + bobGrade + camGrade) / 3;
+console.log(gradeAverage)
+deeGrade = 90
+gradeAverageWithD = (gradeAverage) + deeGrade / 4;
+
+//Problem 6
+//a. 
+
+    let num1 = 2
+    let num2 = 5
+    let num3 = num1 * num2
+    console.log(num3)//10
+
+// b. 
+    
+    let str = 'jel' 
+    str += 'lo'
+    console.log(str)// 'jello'
+    
+//c. 
+
+    let string =  'My favorite number is ';
+    let number = 42
+    let sentence = string + number
+    console.log(typeof(sentence))//string
+    
+    
