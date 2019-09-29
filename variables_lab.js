6b4b7d431015bc9af0bc2ce5e1651791ba78c0f1
patrickgeorgesmith <patricksmith@pursuit.org>
2019-09-29 10:06:17 -0400

    Variables Lab

--- variables_lab.js
+++ variables_lab.js
@@ -1,29 +1,48 @@
-let birthYear = 1995;
-let futureYear = 2050;
-let ageMin =  futureYear - birthYear;
+// Problem One 
+let birthYear = 1991
+let futureYear = 2020
+let ageMin = futureYear - birthYear
 let ageMax =  ageMin - 1;
 console.log('I will either be ' + ageMin + ' or ' + ageMax + ' in ' + futureYear);
 
-// Snack supply calculator
-let crrAge = 23;
-let maxAge = 50;
-let snackDay = 1;
-let favSanck = 'Peanuts';
-let snackYear = snackDay * 365;
-let totalSnack = snackYear * (maxAge - crrAge);
-console.log('You will need ' + totalSnack + ' of ' + favSanck + ' to last you until the age of ' + maxAge); 
 
-//Properties of a circle
-let radius = 7
+// Problem Two
+let currentAge = 28
+let maxAge = 50
+let snackAmount = 3
+let snackYear = 365 * snackAmount
+let overallSnack = snackYear * (maxAge - currentAge)
+console.log("You will need " + overallSnack + " to last you until the age of " + maxAge)
+
+// Problem Three
+let radius = 24
 let diameter = radius * 2
-let circumference = Math.PI * diameter 
-let area = Math.PI * radius * radius
-console.log('The circumference is: ' + circumference);
-console.log('The area is: ' + area);
+let circumferenceResult = Math.PI * diameter 
+let areaofCircle = Math.PI * radius * radius
+console.log('The circumference is: ' + circumferenceResult);
+console.log('The area is: ' + areaofCircle);
 
-// Temperature converter
-let c = 0;
-let cInF = (c * 1.8) + 32;
-let f = 32;
-let fInC = (f - 32) / 1.8;
-console.log(`C = ${c} into F is: ${cInF} ---- F = ${f} into C is: ${fInC}`)
+//Problem Four
+
+let celcius = 0;
+let celciusInFahrenheit= (celcius * 1.8) + 32;
+let fahrenheit = 32;
+let fahrenheitInCelcius = (fahrenheit - 32) / 1.8;
+
+//Problem Five
+
+let aliceGrade = 95
+let bodGrade = 90
+let camGrade = 89
+let gradeAverage = (aliceGrade + bodGrade + camGrade) / (3)
+let deeGrade = 88
+let newAverage = (aliceGrade + bodGrade + camGrade + deeGrade) /(4)
+if (deeGrade > newAverage) {
+  console.log(deeGrade) 
+}
+
+//Problem Five
+1. Output will be 10
+2. Output will be jello
+3. Output will be String
+
