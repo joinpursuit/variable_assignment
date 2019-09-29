503a1229f6b11f0dec8649b69fa465a016f7b878
Rumel <rumelahmed@pursuit.org>
2019-09-29 10:21:05 -0400

    variables lab

--- variables_lab.js
+++ variables_lab.js
@@ -1,29 +1,53 @@
-let birthYear = 1995;
+// Problem One
+let birthYear  = 1985;
 let futureYear = 2050;
-let ageMin =  futureYear - birthYear;
-let ageMax =  ageMin - 1;
-console.log('I will either be ' + ageMin + ' or ' + ageMax + ' in ' + futureYear);
+let currentYear = 2019;
+console.log(futureYear - birthYear);
+let ageMin =  (futureYear - birthYear);
+let ageMax = (ageMin - 1);
+console.log("I will be either " + " " + ageMin + " or" + " " + ageMax + " " + "in" + " " + futureYear);
 
-// Snack supply calculator
-let crrAge = 23;
-let maxAge = 50;
-let snackDay = 1;
-let favSanck = 'Peanuts';
-let snackYear = snackDay * 365;
-let totalSnack = snackYear * (maxAge - crrAge);
-console.log('You will need ' + totalSnack + ' of ' + favSanck + ' to last you until the age of ' + maxAge); 
 
-//Properties of a circle
-let radius = 7
-let diameter = radius * 2
-let circumference = Math.PI * diameter 
-let area = Math.PI * radius * radius
-console.log('The circumference is: ' + circumference);
-console.log('The area is: ' + area);
+//Problem Two
+let age = 34;
+let ageMaxx = 100;
+let snackNumber = 5;
+let snackYear = (ageMaxx - age) * (snackNumber * 365)
+console.log("you will need " + snackYear)
 
-// Temperature converter
-let c = 0;
-let cInF = (c * 1.8) + 32;
-let f = 32;
-let fInC = (f - 32) / 1.8;
-console.log(`C = ${c} into F is: ${cInF} ---- F = ${f} into C is: ${fInC}`)
+
+//Problem Three
+let radius = 5;
+let circumference = 2 * Math.PI * radius;
+let areaOfCircle = Math.PI * radius * radius;
+console.log("The circumference is" + 
+" " + circumference) ;
+console.log ("The area is " + areaOfCircle)
+
+//Problem Four
+let celsius = 12
+let farenheit = (celsius * 1.8) + 32 
+console.log ("tempInFarenheit " + farenheit)
+//
+let tempfarenheit = 75
+let tempcelsius = (tempfarenheit -32) / 1.8
+console.log ("tempInCelsius " + tempcelsius)
+
+//Problem Five
+let aliceGrade = 75
+let bobGrade = 85
+let camGrade = 95
+let classAverage = (aliceGrade + bobGrade + camGrade) / 3
+let deeGrade = 100
+let newClassAverage = (aliceGrade + bobGrade + camGrade + deeGrade) / 4
+console.log("class average " + classAverage)
+console.log("all class average " + newClassAverage)
+if (deeGrade>newClassAverage){console.log("Dee grade is higher than the class average")} else{console.log("Dee did not have the highest grades in the class")}
+
+
+
+//Problem Six
+a. 10
+b. jello
+c. string
+
