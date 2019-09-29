a991205f8dbca33f7f88ce6b00f557fbabfd9746
Kelvin <kelvinarellano@pursuit.org>
2019-09-29 06:09:45 -0400

    finished

--- variables_lab.js
+++ variables_lab.js
@@ -1,15 +1,67 @@
-let birthYear = 1991
-let midlife = 2032
-let agemin = (midlife - birthYear)
-let agemax = (agemin + 1)
-console.log("I will be either " + agemin + " or " + agemax + " in " + midlife)
+let birthYear = 1991;
+let midlife = 2032;
+let agemin = (midlife - birthYear);
+let agemax = (agemin + 1);
+console.log("I will be either " + agemin + " or " + agemax + " in " + midlife);
 
 
 
 
 
 
-let currentAge = 27 
-let futureAge = 37
-let dailySnacks = 3
-let snackNumber = (futureAge - currentAge)
+let currentAge = 27; 
+let futureAge = 37;
+let dailySnacks = 3;
+let snackNumber = ((futureAge - currentAge) + (dailySnacks * 365));
+console.log("You will need " + snackNumber + " to last you until the age of " + futureAge)
+
+
+
+let radius = 4;
+let pi = (Math.PI);
+let circumferenceResult = (2 * pi * radius);
+console.log("The circumference is " + circumferenceResult);
+let areaOfaCircle = pi * radius * radius;
+console.log("The area of a circle is " + areaOfaCircle);
+
+
+
+let celsiusTemp = 32;
+let fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+console.log(celsiusTemp + " degrees C is " + fahrenheitTemp + " degrees F"); 
+let newFahrenheitTemp = 82; 
+let newCelsiusTemp = (newFahrenheitTemp - 32) * 5/9;
+console.log(newFahrenheitTemp + " degrees F is " + newCelsiusTemp + " degrees C");
+
+
+
+let aliceGrade = 80
+let bodGrade = 75
+let camGrade = 85
+let classCurve = (camGrade + bodGrade + camGrade)/3
+let deeGrade = 90
+let newClassCurve = (camGrade + bodGrade + camGrade + deeGrade)/4
+if 
+    (deeGrade > newClassCurve) 
+    console.log(deeGrade)
+
+
+
+
+
+
+
+let num1 = 2 
+let num2 = 5
+let num3 = (num1 * num2)
+console.log(num3)
+//10
+let str = "jel" 
+str += "lo" 
+console.log(str)
+//"prints the word jello"
+let string = "My favorite number is "
+let number = 42
+let sentence = string + number
+console.log(typeof(sentence))
+//string
