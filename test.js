64f99b8cea30159fef749b0b169a9d7dceabdfb0
crystal cardona <crystalcardona@pursuit.org>
2019-09-29 10:22:28 -0400

    variables lab

---
+++ test.js
@@ -0,0 +1,41 @@
+let birthYear = 1992;
+let futureYear = 2050;
+console.log(futureYear - birthYear)
+let ageMin = (futureYear - birthYear)
+let ageMax = (ageMin -1)
+console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear);
+
+let age = 28;
+let maxAge = 100;
+let snackDay = 5;
+let snackYear = (maxAge - age) * (snackDay* 365)
+console.log("you will need " + snackYear)
+
+let radius = 3;
+let circumferenceResult = 2 * Math.PI * radius;
+console.log("The circumference is " + circumferenceResult)
+let areaOfCircle = Math.PI * (radius * radius);
+console.log("the area is " + areaOfCircle);
+
+let tempInCelcius = 15;
+let tempInFahrenheit = (tempInCelcius * 1.8) + 32;
+console.log(tempInFahrenheit);
+
+
+
+
+let aliceGrade = 75
+let bobGrade= 85
+let camGrade = 95
+let classAverage = (aliceGrade + bobGrade + camGrade) / 3
+console.log(classAverage)
+let deeGrade = 100
+console.log("Alice, Bob , and Cams average is " + classAverage)
+let newClassAverage = (aliceGrade + bobGrade + camGrade + deeGrade) / 4
+console.log("The avearge for the whole class is " + newClassAverage)
+if (deeGrade > newClassAverage) {console.log("Dee has the highest grade in the class")} else{console.log("Dee did not have the highest grades in class")}
+
+//
+a. 10
+b. jello
+c. string
