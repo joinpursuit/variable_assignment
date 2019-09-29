b40c6057e00778fdffad96af52ed4f1e971204c2
david maravillas <davidmaravillas@pursuit.org>
2019-09-29 08:41:29 -0400

    var done:

--- variables_lab.js
+++ variables_lab.js
@@ -1,29 +1,64 @@
-let birthYear = 1995;
-let futureYear = 2050;
-let ageMin =  futureYear - birthYear;
-let ageMax =  ageMin - 1;
-console.log('I will either be ' + ageMin + ' or ' + ageMax + ' in ' + futureYear);
+//problem one
+const birthYear = 1986
+let futureYear = 2026
+let ageMin = futureYear-(birthYear+1)
+let ageMax = (futureYear-birthYear)
+console.log (futureYear-birthYear)
+console.log ("I will be either "+ageMin+ " or " + ageMax+" in "+futureYear)
 
-// Snack supply calculator
-let crrAge = 23;
-let maxAge = 50;
-let snackDay = 1;
-let favSanck = 'Peanuts';
-let snackYear = snackDay * 365;
-let totalSnack = snackYear * (maxAge - crrAge);
-console.log('You will need ' + totalSnack + ' of ' + favSanck + ' to last you until the age of ' + maxAge); 
+//problem two
+let currentAge = 33
+let maxAge = 40
+let dailySnack = 3
+let snackNumber = dailySnack*365*(maxAge-currentAge)
 
-//Properties of a circle
-let radius = 7
-let diameter = radius * 2
-let circumference = Math.PI * diameter 
-let area = Math.PI * radius * radius
-console.log('The circumference is: ' + circumference);
-console.log('The area is: ' + area);
+console.log ("You will need "+snackNumber+" to last you until the age of "+maxAge)
 
-// Temperature converter
-let c = 0;
-let cInF = (c * 1.8) + 32;
-let f = 32;
-let fInC = (f - 32) / 1.8;
-console.log(`C = ${c} into F is: ${cInF} ---- F = ${f} into C is: ${fInC}`)
+//problem three
+let radius = 6
+let circumferenceResult = ((2*radius)*Math.PI)
+let areaOfCircle = Math.PI*Math.pow(radius,2)
+
+console.log("The circumference is "+circumferenceResult+".")
+console.log("The area is "+areaOfCircle+".")
+
+//Problem four
+let tempInCelcius = 40
+let tempInFahrenheit  = (tempInCelcius*(9/5))+32
+
+console.log(tempInCelcius+"°C is "+tempInFahrenheit+"°F")
+
+let tempInFahrenheit2  = 104
+let tempInCelcius2 = (tempInFahrenheit-32)*(5/9)
+
+console.log(tempInFahrenheit2+"°F is "+tempInCelcius2+"°C."
+)
+
+//PROBLEM FIVE
+let aliceGrade = 90
+let bobGrade = 80
+let camGrade = 70
+let gradeAvg1 = (aliceGrade+bobGrade+camGrade)/3
+
+console.log("The classe average is "+gradeAvg1)
+
+let deeGrade = 85
+let gradeAvg2 = (aliceGrade+bobGrade+camGrade+deeGrade)/4
+console.log("The classe average with Dee is "+gradeAvg2)
+if(deeGrade>gradeAvg2){console.log("Dee's grade is higher than the class average")} else {console.log("Dee's grade is lower than the class average")}
+
+//Problem six
+//6.A 10
+let num1 = 2
+    let num2 = 5
+    let num3 = num1 * num2
+    console.log(num3) //10
+//6.B 
+let str = 'jel' 
+    str += 'lo'
+    console.log(str)//JEL
+//6.C
+let string =  'My favorite number is ';
+let number = 42
+let sentence = string + number
+console.log(typeof(sentence)) // STRING
