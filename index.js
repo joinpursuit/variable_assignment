729d7722ba0fda035b90f661b422c6bab715f3b6
cassidybeni <cassidybeni@pursuit.org>
2020-03-04 00:40:54 -0500

    Change ageMin to ageMax - 1

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 let birthYear = 1996
 let futureYear = 2020
-let ageMin = (futureYear - birthYear) - 1
 let ageMax = futureYear - birthYear
+let ageMin = ageMax - 1
 
 console.log('I will be either ' + ageMin + ' or ' + ageMax + ' in ' + futureYear)
+
