c6cc0918e9c2b0bf6a4287bd1a3aa8b922ee3af8
cassidybeni <cassidybeni@pursuit.org>
2020-12-08 14:35:44 -0500

    Complete problem 1

---
+++ problems/index.js
@@ -0,0 +1,6 @@
+//Problem 1
+const birthYear = 1996
+let futureYear = 2021
+let ageMax = futureYear - birthYear
+let ageMin = ageMax - 1
+console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear)
