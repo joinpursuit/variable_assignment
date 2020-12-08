9d2ab111a5a665cca9d87df4161bf530a1881193
cassidybeni <cassidybeni@pursuit.org>
2020-12-08 14:51:45 -0500

    Complete problem 2

--- problems/index.js
+++ problems/index.js
@@ -3,4 +3,11 @@ const birthYear = 1996
 let futureYear = 2021
 let ageMax = futureYear - birthYear
 let ageMin = ageMax - 1
-console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear)
+console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear + ".")
+
+//Problem 2
+let currentAge = 24
+const maxAge = 100
+let snackPerDay = 2
+let snackNumber = (snackPerDay * 365)*(maxAge - currentAge)
+console.log("You will need " + snackNumber + " to last you until the age of " + maxAge + ".")
