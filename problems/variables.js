c3198a0e9098b3d4d0575b69959e89e2b55c0836
Raymond Udeogu <rfizzle25@gmail.com>
2020-12-08 17:02:48 -0500

    not finished

--- problems/variables.js
+++ problems/variables.js
@@ -6,6 +6,23 @@
 //  * Calculate your 2 possible ages for that year based on the stored values.
 //  * For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
 //  * Log them to the screen like so: "I will be either `ageMin` or `ageMax` in `futureYear`", substituting the values.
+// node problems/variables.js
+
+const birthYear = 2000
+
+let futureYear = 2010
+
+let ageMin = futureYear - birthYear
+
+let ageMax = (futureYear - birthYear) + 1
+
+
+console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear)
+
+
+
+
+
 
 // ## Problem Two
 
@@ -16,6 +33,16 @@
 //  * Calculate how many snacks you would eat total, from your current age until the maximum age.
 //  * Log the result to the screen like this: "You will need `snackNumber` to last you until the age of `maxAge`".
 
+let currentAge = 29
+
+const maximumAge = 35
+
+let snackPerDay = 10
+
+let snackNumber = ((maximumAge - currentAge) * (snackPerDay * 365))
+
+console.log("You will need " + snackNumber + " to last you until the age of " + maximumAge)
+
 // ## Problem Three
 
 // Calculate properties of a circle, using the definitions: http://math2.org/math/geometry/circles.htm 
@@ -24,6 +51,23 @@
 //  * Calculate the area based on the radius, and log "The area is `areaOfCircle`".
 //  * Hint: https://www.w3schools.com/jsref/jsref_pi.asp
 
+let radiusOfCircle = 3
+
+let diameterOfCircle = radiusOfCircle * 2
+
+let circumferenceResult = 3.14 * diameterOfCircle
+
+let areaOfCircle = 3.14 * (radiusOfCircle)**2
+
+console.log("The circumference is" + circumferenceResult)
+
+console.log("The area is " + areaOfCircle)
+
+
+
+
+
+
 // ## Problem Four
 
 // Temperature Converter:
@@ -31,6 +75,18 @@
 //  * Convert it to fahrenheit and output "`tempInCelsius`°C is `tempInFahrenheit`°F".
 //  * Now store a fahrenheit temperature into a variable.
 //  * Convert it to celsius and output "`tempInFahrenheit`°F is `tempInCelsius`°C."
+
+let tempInCelsius = 10
+
+let tempInFahrenheit = (tempInCelsius * 9/5) + 32
+
+console.log(tempInCelsius + "C is " + tempInFahrenheit + "F")
+
+let tempInFahrenheit1 = 10
+
+let tempInCelsius1 = (tempInFahrenheit1 - 32) * 5/9
+
+console.log(tempInFahrenheit1 + "F is " + tempInCelsius1 + "C")
 
 
 // ## Problem Five
@@ -44,6 +100,23 @@
 //  * Find the average grade of all students
 //  * Print out if Dee's grade is higher than the class average
 
+let alice = 60
+
+let bob = 70
+
+let cam = 80
+
+let classAverage = (alice + bob + cam) / 3
+
+let dee = 90 
+
+let newClassAverage = (alice + bob + cam + dee) / 4
+
+console.log(newClassAverage <= dee)
+
+
+
+
 // ## Problem Six
 
 // Find the last number:
@@ -56,6 +129,7 @@
 
 // Hint:
 // Use the remainder % operator.
+
 
 
 // ## Problem Seven
@@ -82,6 +156,16 @@
 // * alice + x = y * (bob + x)
 // * Solve for alice
 
+x = 3
+
+y = 2
+
+bob1 = 12
+
+aliceAge = y * ((bob1 + x) / x) 
+
+console.log(aliceAge)
+
 // ## Problem Eight
 
 // * Cat and Dog Percentages
@@ -106,6 +190,11 @@
 // * Years that are divisible by 100 are not leap years if they are not also divisible by 400. 
 // * For example 1900 was not a leap year, but 2000 was. Print "Leap year!" or "Not a leap year!" depending on the year you are provided.
 
+let leapYear =2020
+
+let isLeapYear = leapYear % 4 && (leapYear % 100 && leapYear % 400)
+
+console.log((isLeapYear === 0 && "Leap Year!") || isLeapYear !== 0 "Not a leap year")
 
 // ## Problem Ten: Predict the output
 
