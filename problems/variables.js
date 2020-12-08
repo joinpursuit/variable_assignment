68b3f91407bc0d58e71ac3e27321f619be5a0a81
Mendoza Holger <mendozadavidh111@gmail.com>
2020-12-08 15:43:54 -0500

    add changes to variable.js

--- problems/variables.js
+++ problems/variables.js
@@ -7,6 +7,14 @@
 //  * For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
 //  * Log them to the screen like so: "I will be either `ageMin` or `ageMax` in `futureYear`", substituting the values.
 
+const birthYear = 1997
+let futureYear = 2077
+
+ageMin = futureYear - birthYear
+
+ageMin = 80
+let ageMax = 81
+
 // ## Problem Two
 
 // Snack Supply Calculator:
@@ -16,6 +24,15 @@
 //  * Calculate how many snacks you would eat total, from your current age until the maximum age.
 //  * Log the result to the screen like this: "You will need `snackNumber` to last you until the age of `maxAge`".
 
+let myAge = 23
+const maxAge = 70
+let snackPerDay = 5
+
+snackNumber = ((snackPerDay * 365) * 47)
+//snackNumber = 85775
+
+"You will need 85775 to last you until the age of 70"
+
 // ## Problem Three
 
 // Calculate properties of a circle, using the definitions: http://math2.org/math/geometry/circles.htm 
@@ -23,6 +40,16 @@
 //  * Calculate the circumference based on the radius, and log "The circumference is `circumferenceResult`".
 //  * Calculate the area based on the radius, and log "The area is `areaOfCircle`".
 //  * Hint: https://www.w3schools.com/jsref/jsref_pi.asp
+
+let radius = 12
+//formula for circumference is PI * diameter *diameter is 2x the radius*
+circumference = (24 * 3.14)
+circumference = 75.39
+"the Circumference is 75.39"
+//formula for area is PI * r^2
+area = (3.14 * 144)
+area = 452.16
+"The area is 452.16"
 
 // ## Problem Four
 
@@ -32,7 +59,13 @@
 //  * Now store a fahrenheit temperature into a variable.
 //  * Convert it to celsius and output "`tempInFahrenheit`°F is `tempInCelsius`°C."
 
+let celcius = 22
+// faranheit = (22 * 2 + 32)
+"22 ceclius is 76 in faranheit"
 
+let faranheit = 60
+celcius = (60 / 2 - 32) 
+"60 in faranheit is -32 in celcius"
 // ## Problem Five
 
 // Grades Calculator:
@@ -44,6 +77,17 @@
 //  * Find the average grade of all students
 //  * Print out if Dee's grade is higher than the class average
 
+let aliceGrade = 90
+let bobGrade = 83
+let camGrade = 85
+
+averageGrade = ((100 * aliceGrade) + (100 * bobGrade) + (100 * camGrade) /  (100 + 100 + 100)) / 300
+averageGrade = 86 
+
+let deeGrade = 82
+
+averageGrade = ((100 * aliceGrade) + (100 * bobGrade) + (100 * camGrade) + (100 * deeGrade) / (100 + 100 + 100 + 100)) / 400
+
 // ## Problem Six
 
 // Find the last number:
@@ -53,6 +97,11 @@
 // * a = 123
 // * Output:
 // * 3
+
+Input:
+a = 4876
+Output:
+6
 
 // Hint:
 // Use the remainder % operator.
@@ -82,6 +131,12 @@
 // * alice + x = y * (bob + x)
 // * Solve for alice
 
+ x = 2
+y = 3
+bob = 12
+alice = 30
+
+
 // ## Problem Eight
 
 // * Cat and Dog Percentages
@@ -95,6 +150,11 @@
 // * 25% of the daycare animals are cats
 // * 75% of the daycare animals are dogs
 
+numberOfCats = 10
+numberOfDogs = 75
+
+"20% of the daycare are cats, 80% of the daycare are dogs"
+
 // ## Problem Nine
 
 // * Leap Year Calculator
@@ -105,7 +165,7 @@
 // * The above rule is valid except that every 100 years special rules apply. 
 // * Years that are divisible by 100 are not leap years if they are not also divisible by 400. 
 // * For example 1900 was not a leap year, but 2000 was. Print "Leap year!" or "Not a leap year!" depending on the year you are provided.
-
+console.log("Leap Year!")
 
 // ## Problem Ten: Predict the output
 
@@ -116,14 +176,14 @@
 //     let num1 = 2
 //     let num2 = 5
 //     let num3 = num1 * num2
-//     console.log(num3)
+//     console.log(num3) // console.log('10')
 //     ```
 
 // b. 
 //     ```js
 //     let str = 'jel' 
 //     str += 'lo'
-//     console.log(str)
+//     console.log(str) // console.log('jello')
 //     ```
 
 // c. 
@@ -131,7 +191,7 @@
 //     let string =  'My favorite number is ';
 //     let number = 42
 //     let sentence = string + number
-//     console.log(typeof(sentence))
+//     console.log(typeof(sentence)) // console.log('NaN')
 //     ```
 
 
