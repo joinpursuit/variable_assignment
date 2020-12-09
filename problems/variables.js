0738479c553293078c074788261702efe22bea6f
Hady Mohamed <hadymohamed@pursuit.org>
2020-12-08 23:36:33 -0500

    need help with questions 7,8,& 9

--- problems/variables.js
+++ problems/variables.js
@@ -7,6 +7,13 @@
 //  * For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
 //  * Log them to the screen like so: "I will be either `ageMin` or `ageMax` in `futureYear`", substituting the values.
 
+    const birthYear = 1987
+    let futureYear = 2063
+    const ageMin = 75
+    const ageMax = 76
+    console.log("I will be either " + ageMin + " or " + ageMax + " in " + futureYear)
+
+
 // ## Problem Two
 
 // Snack Supply Calculator:
@@ -16,6 +23,16 @@
 //  * Calculate how many snacks you would eat total, from your current age until the maximum age.
 //  * Log the result to the screen like this: "You will need `snackNumber` to last you until the age of `maxAge`".
 
+    let myAge = 33
+    const maxAge = 100
+    let daysPerYear = 365
+    let snacksPerDay = 3
+    let snackNumber = (daysPerYear * (maxAge - myAge) * snacksPerDay)
+    //Calculation: 365 days * 67 years * 3 snacks a day = 73,365 snacks
+    console.log("You will need " + snackNumber + " snacks to last you until the age of " + maxAge)
+
+
+
 // ## Problem Three
 
 // Calculate properties of a circle, using the definitions: http://math2.org/math/geometry/circles.htm 
@@ -23,6 +40,16 @@
 //  * Calculate the circumference based on the radius, and log "The circumference is `circumferenceResult`".
 //  * Calculate the area based on the radius, and log "The area is `areaOfCircle`".
 //  * Hint: https://www.w3schools.com/jsref/jsref_pi.asp
+
+const circleRadius = 10
+const circleDiameter = 20
+// Diameter is 2 * circleRadius
+const circumferenceResult = Math.PI * circleDiameter
+console.log("The circumference is " + circumferenceResult)
+const areaOfCircle = Math.PI * (Math.pow(circleRadius, 2))
+// Area = Pi * radius squared
+console.log("The area is " + areaOfCircle)
+
 
 // ## Problem Four
 
@@ -32,6 +59,13 @@
 //  * Now store a fahrenheit temperature into a variable.
 //  * Convert it to celsius and output "`tempInFahrenheit`°F is `tempInCelsius`°C."
 
+let tempInCelsius = 50
+let tempInFahrenheit = (tempInCelsius * 1.8) + 32
+console.log(tempInFahrenheit)
+
+let tempFahrenheit = 120
+let tempCelsius = (tempFahrenheit - 32)/1.8
+console.log(tempCelsius)
 
 // ## Problem Five
 
@@ -43,6 +77,17 @@
 //  * Store Dee's grade on a test to a variable
 //  * Find the average grade of all students
 //  * Print out if Dee's grade is higher than the class average
+
+let alicesGrade = 97
+let bobsGrade = 100
+let camsGrade = 88
+let averageStudentGrade = (alicesGrade + bobsGrade + camsGrade)/3
+console.log("The average grade of all 3 students is " + averageStudentGrade)
+let deesGrade = 100
+let newStudentAverageGrade = (alicesGrade + bobsGrade + camsGrade + deesGrade)/4
+console.log("The new average of all 4 students is " + newStudentAverageGrade)
+if (newStudentAverageGrade > averageStudentGrade){ console.log("Dee's Score  96.25 ")}
+
 
 // ## Problem Six
 
@@ -57,6 +102,7 @@
 // Hint:
 // Use the remainder % operator.
 
+console.log(123 % 3)
 
 // ## Problem Seven
 
@@ -116,22 +162,33 @@
 //     let num1 = 2
 //     let num2 = 5
 //     let num3 = num1 * num2
-//     console.log(num3)
+//     console.log(num3)  
 //     ```
+    let num1 = 2
+    let num2 = 5
+    let num3 = num1 * num2
+    console.log(num3) //10
 
 // b. 
 //     ```js
 //     let str = 'jel' 
 //     str += 'lo'
-//     console.log(str)
+//     console.log(str) 
 //     ```
+    let str = 'jel'
+    str += 'lo'
+    console.log(str) //jello
+
 
 // c. 
 //     ```js
 //     let string =  'My favorite number is ';
 //     let number = 42
 //     let sentence = string + number
-//     console.log(typeof(sentence))
+//     console.log(typeof(sentence))   
 //     ```
 
-
+let string = 'My favorite number is'
+let number = 42
+let sentence = string + number
+console.log(typeof(sentence)) //String
